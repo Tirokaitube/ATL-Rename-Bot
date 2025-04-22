a90a7b466de1fc3ba5c0c333c0c2a3c8531ad67b
Tirokaitube <tirokaitube@gmail.com>
2025-04-22 23:04:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ API_HASH = os.environ.get("API_HASH", "")
 #BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 TOKEN_ONE = os.environ.get("TOKEN_ONE", "")
 
-CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
-BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","BYNF_TamilChat") # username without '@'
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","ATL_Univers") # username without '@'
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME","HMF_Owner_1")
-STRING = os.environ.get("STRING", "")
+CHANNEL = os.environ.get("CHANNEL", "Anime_Lumino") # username without '@'
+BOT_USERNAME = os.environ.get("BOT_USERNAME","V2_File_Renamer_bot") # username without '@'
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","Anime_Chat_Group_V2") # username without '@'
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","Anime_Lumino") # username without '@'
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME","I_am_Babi_villain")
+STRING = os.environ.get("STRING", "BQGlhLEARnzKQUO9fhjjo65GWUVbf2OuowzqJYhqZDUclW029UEa4ix4Zl8ejr0pL8jdPkdCxPN8fgWzBxNSBniIGxfiqdvziRNweZ_PlI__1jsEVnD65pgLzdz8-ugPg8W5Fy5AGabAEzZ63AsLCIZk7DerIgcFx_NwEkjJ7y19eRYWybaHdFhVvPhjvVniCIRdVgI6Fc-dc9dztE2ZWRYWKjkb0wgPVRdgJwSRy5AtFmbHyeLfwsvRUkGUmES9jjVxzPZI2lxXu6D_-Rik0ryL0J02LvJoixGwYfXTcV3v3oh6S3jCEEdJtPDHKUsrLWDuRGZ7xcwrqNrIVt7ulkqauJE6FQAAAAGdpDQAAA")
 
-DB_NAME = os.environ.get("DB_NAME","renameone")     
-DB_URL = os.environ.get("DB_URL","")
+DB_NAME = os.environ.get("DB_NAME","tirokaitube")     
+DB_URL = os.environ.get("DB_URL","mongodb+srv://tirokaitube:ymDe3VLWXQ05JazI@cluster0.tktl52k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 FLOOD = int(os.environ.get("FLOOD", "90"))
-LAZY_PIC = os.environ.get("LAZY_PIC", "")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1989750989').split()]
+LAZY_PIC = os.environ.get("LAZY_PIC", "https://i.ibb.co/PvHsTVXC/x.jpg")
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7823475125').split()]
 PORT = os.environ.get('PORT', '8080')
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001230390281"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002568229104"))
